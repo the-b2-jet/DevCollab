c034e574537022b48416f02a959bd262efba9cd7
the-b2-jet <adoniyasshimelis110@gmail.com>
2026-06-12 12:32:09 +0300

    Init commit: Express server running

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('DevCollab is running!');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on : http://localhost:${PORT}`));
